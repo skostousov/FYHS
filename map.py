52b9ad840550bf2d4dbfc8a307c693d994bc366b
skostousov <93064193+skostousov@users.noreply.github.com>
2024-03-18 19:49:45 +0000

    dfgsgs

--- map.py
+++ map.py
@@ -1,6 +1,8 @@
 import folium
 import webbrowser
-
+import os
+import time
+import subprocess
 
 #with the help of Github Copilot
 class Map:
@@ -17,6 +19,7 @@ class Map:
   def update_html(self):
     m = folium.Map(location=[self.x, self.y], zoom_start=self.zoom)
     for entry in self.entrylist:
+      self.poptext = "{}:{}".format(entry['id'], entry['title'])
       folium.CircleMarker(location=[int(entry["lat"]),
                                     int(entry["long"])],
                           radius=entry["radius"],
@@ -24,9 +27,17 @@ class Map:
                           fill=True,
                           fill_color=entry["type"],
                           fill_opacity=entry["transparency"],
-                          popup=folium.Popup(entry['id'])).add_to(m)
+                          popup=folium.Popup(self.poptext)).add_to(m)
     m.save('map.html')
 
-  def run(self):
-    import os
-    os.system("start chrome map.html")
+  def run(self):#with the help of ai
+    server_process = subprocess.Popen(["python3", "-m", "http.server", "8080"])
+
+        # Wait for a few seconds to ensure the server has started
+    time.sleep(3)
+        # Open the HTML file in the preview browser
+    os.system("xdg-open http://localhost:8080/map.html &")
+    input("Press Enter to stop the server...")
+    # Terminate the server process
+    server_process.terminate()
+
